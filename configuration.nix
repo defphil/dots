2b18ee5d375ed739207c7ad831139d45a7ca1ce8
Filip Miletic <filip.miletic@me.com>
2019-11-06 16:00:13 +0100

    stable system finally

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -9,117 +5,103 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
+  boot.kernelModules = [ "iwlwifi" ];
+  boot.extraModprobeConfig = ''
+      options iwlwifi bt_coex_active=0 power_save=Y 11n_disable=8 wd_disable=1 
+    '';
   nixpkgs.config.allowUnfree = true;
+  hardware.enableRedistributableFirmware = true;
 
-  # Use the systemd-boot EFI boot loader.
-  boot = {
-  cleanTmpDir = true; 
-  loader.systemd-boot.enable = true;
-  loader.efi.canTouchEfiVariables = true;
-  initrd.luks.devices = [ {
-        name = "root";
-        device = "/dev/sda2";
-        preLVM = true;
-      }
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.luks.devices = [
+    {
+      name = "root";
+      device = "/dev/sda2";
+      preLVM = true;
+    }
   ];
 
-  };
-    networking.hostName = "nix"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
+  networking.hostName = "freni"; # Define your hostname.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.useDHCP = false;
   networking.interfaces.enp0s25.useDHCP = true;
   networking.interfaces.wlp3s0.useDHCP = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
   i18n = {
     consoleFont = "Lat2-Terminus16";
     consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
   };
 
-  # Set your time zone.
   time.timeZone = "Europe/Belgrade";
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    wget 
-    gcc
-    gdb
-    fzf
-    htop
-    p7zip
-    unrar
-    unzip
-    valgrind
-    vlc
-    xdg-user-dirs
-    zip
-    emacs
-    tdesktop
-    tmux
+    cargo
     firefox
+    emacs
     git
-    ripgrep
-    networkmanagerapplet
-    nix-prefetch-scripts
-    gnumake
-    gnupg
+    openjdk11
+    leiningen
+    compton
+    rxvt_unicode-with-plugins     
+    wget
     vim
-    which    
+    which
     zsh
-    weechat
-    cmake
-    dbus
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
+  fonts.fonts = with pkgs; [
+    terminus_font
+    liberation_ttf
+  ];
+
   programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-
-  # services.openssh.enable = true;
-
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Enable CUPS to print documents.
-  services = {
-    locate.enable = true;
-    printing.enable = true;
-  };
-  # Enable sound.
+  services.printing.enable = true;
   sound.enable = true;
   hardware.pulseaudio.enable = true;
-# 
-   services.xserver = {
-     enable = true;
-     autorun = false;
-     layout = "us";
-     xkbOptions = "ctrl:nocaps";
-     libinput.enable = true;
-     displayManager.sddm.enable = true;
-     desktopManager.default = "plasma5";
-     desktopManager.plasma5.enable = true;
-   };
-	
-  users = {     defaultUserShell = pkgs.zsh;  };
+
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "ctrl:nocaps";
+  services.xserver = {
+    autoRepeatDelay = 200;
+    autoRepeatInterval = 50;
+
+    desktopManager = {
+      default = "none";
+      xterm.enable = false; 
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu
+        i3status-rust
+      ];
+    };
+  };
+  services.compton.enable=true;
+
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-media-driver # only available starting nixos-19.03 or the current nixos-unstable
+    ];
+  };
+
+  services.xserver.libinput.enable = true;
+
   users.users.phil = {
     isNormalUser = true;
-    group = "users";
-    shell = pkgs.zsh;
-    extraGroups = [ "wheel" "video" "audio" "networkmanager" "disk" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "audio" "video" "disk" ]; # Enable ‘sudo’ for the user.
   };
 
   system.stateVersion = "19.09"; # Did you read the comment?
-
 }
 
