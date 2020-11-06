d754af36f5059cbcc3d31e47ba6afd63fba40ef2
defphil <filip.miletic@me.com>
2020-11-06 08:22:30 +0100

    cleanup

--- i3/.xsetroot.sh
+++
@@ -1,6 +0,0 @@
-DATETIME=`date`
-UPTIME=`uptime | sed 's/.*up\s*//' | sed 's/,\s*[0-9]* user.*//' | sed 's/  / /g'`
-VOLUME=$( amixer sget Master | grep -e 'Front Left:' | \
-    sed 's/[^\[]*\[\([0-9]\{1,3\}%\).*\(on\|off\).*/\2 \1/' | sed 's/off/M/' | sed 's/on //' )
-BATTERYSTATE=$( acpi -b | awk '{ split($5,a,":"); print substr($3,0,2), $4, "["a[1]":"a[2]"]" }' | tr -d ',' )
-xsetroot -name "VOL: ${VOLUME} | ${DATETIME} | Up ${UPTIME} | ${BATTERYSTATE}"
